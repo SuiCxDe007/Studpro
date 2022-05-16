db585ab1124cd4e4e17e7e845778fcb058ba060f
Manjitha <Manjitha456@gmail.com>
2022-05-16 09:02:01 +0530

    Added firebase and build a CRUD

--- src/App.js
+++ src/App.js
@@ -1,23 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Container, Navbar,Nav } from 'react-bootstrap';
+
+import "./App.css";
+import { db} from "./firebase-config";
+// import storage from './firebase';
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 function App() {
+  
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
+
+  const [users, setUsers] = useState([]);
+  const [companys, setCompanys] = useState([]);
+
+  const usersCollectionRef = collection(db, "users");
+  const CompanyColltectionRef  = collection(db,"Sponsors");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  };
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "users", id);
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+  };
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    const getCompany = async () => {
+      const data = await getDocs(CompanyColltectionRef);
+      setCompanys(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+    getCompany();
+  }, [usersCollectionRef,CompanyColltectionRef]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="light" variant="light">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <input
+        placeholder="Name..."
+        onChange={(event) => {
+          setNewName(event.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Age..."
+        onChange={(event) => {
+          setNewAge(event.target.value);
+        }}
+      />
+
+      <button onClick={createUser}> Create User</button>
+      {users.map((user) => {
+        return (
+          <div>
+            {" "}
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            
+            <button
+              onClick={() => {
+                updateUser(user.id, user.age);
+              }}
+            >
+              {" "}
+              Increase Age
+            </button>
+            <button
+              onClick={() => {
+                deleteUser(user.id);
+              }}
+            >
+              {" "}
+              Delete User
+            </button>
+          </div>
+        );
+      })}
+      {/* <img 
+      src="https://firebasestorage.googleapis.com/v0/b/website-3feaf.appspot.com/o/4f32b-16486378788659-1920.webp?alt=media&token=2963b3a3-4476-458e-aaed-c2513760344b"
+      alt="new"
+      /> */}
+      {companys.map((company) => {
+        return (
+          <div>
+            {" "}
+            <h1>Name: {company.name}</h1>
+            <img
+            src={company.logo}
+            alt="new"
+            />
+            
+          </div>
+        );
+      }
+      )}
+      {/* <h1>vyvhk</h1> */}
+      
+      
     </div>
   );
 }
